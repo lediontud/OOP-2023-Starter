5e96ee1231bffe44428117c762d9372505998a8b
lediontud <c21317311@mytudublin.ie>
2023-04-26 17:42:47 +0100

    final touches

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -60,7 +60,7 @@ public class Main
 	public static void dani() {
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new DANI());
-    }
+    } 
 
 	
 	public static void main(String[] args) {
